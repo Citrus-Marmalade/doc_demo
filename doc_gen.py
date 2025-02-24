7d3225956cdcb13e284d2278a24b714a2f2c7f3e
Gabriel B <gab.beaulieu@proton.me>
2025-02-24 12:18:46 -0500

    CPC - Basic documentation demo
    * CPC - Basic documentation
    
    * CPC push build folder
    
    * Add documenation genration script
    
    * Update doc sources file and add nxp port page
    
    * Update docs build
    
    * fixup! Add documenation genration script
    
    * fixup! Update docs build
    
    * update documentation

---
+++ doc_gen.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+import subprocess
+import webbrowser
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate and optionally view Sphinx documentation.')
+parser.add_argument('-v', '--view', action='store_true', help='View the generated documentation in a web browser')
+
+# Define the docs directory
+docs_dir = 'docs'
+
+# Clean the docs directory
+if os.path.exists(docs_dir):
+    shutil.rmtree(docs_dir)
+os.makedirs(docs_dir)
+
+# Regenerate the Sphinx documentation
+subprocess.run(['sphinx-build', '-b', 'html', '.', docs_dir])
+
+# add nojekyll in build root folder.
+with open(os.path.join(docs_dir, '.nojekyll'), 'w') as f:
+    pass
+
+# Open the generated documentation in a web browser
+index_path = os.path.join(docs_dir, 'index.html')
+args = parser.parse_args()
+
+if args.view:
+    webbrowser.open(f'file://{os.path.abspath(index_path)}')
