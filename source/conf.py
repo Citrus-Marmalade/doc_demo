9de2fcd7b25cfd7c83b4a1d49a74cc1f68ea21f2
Gabriel Beaulieu <gabriel.beaulieu@silabs.com>
2025-02-26 09:59:52 -0500

    bump version number to 0.0.4

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'demo_doc'
 copyright = '2025, Gabriel Beaulieu'
 author = 'Gabriel Beaulieu'
-release = '0.0.3'
+release = '0.0.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
